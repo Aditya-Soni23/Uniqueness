98c5a7a47be1eddaa48c81d6d49d24ce80a288bc
Aditya Soni <77341618+Aditya-Soni23@users.noreply.github.com>
2023-12-14 20:53:57 +0530

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,115 @@
+function getcat(){
+    window.location = "catalogs.html";
+}
+function home(){
+    window.location = "index.html";
+}
+function con(){
+    window.location = "cont.html";
+}
+function sb(){
+    window.location = "buyandsell.html";
+}
+
+
+
+
+
+
+function buynoww(){
+    var t= document.getElementById("itemname");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
+
+function buynoww2(){
+    var t= document.getElementById("itemname2");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount2");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
+function buynoww3(){
+    var t= document.getElementById("itemname3");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount3");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
+function buynoww4(){
+    var t= document.getElementById("itemname4");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount4");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
+function buynoww5(){
+    var t= document.getElementById("itemname5");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount5");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
+function buynoww6(){
+    var t= document.getElementById("itemname6");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount6");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
+function buynoww7(){
+    var t= document.getElementById("itemname7");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount7");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
+function buynoww8(){
+    var t= document.getElementById("itemname8");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount8");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
+function buynoww9(){
+    var t= document.getElementById("itemname9");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount9");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
+function buynoww10(){
+    var t= document.getElementById("itemname10");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount10");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
+function buynoww11(){
+    var t= document.getElementById("itemname11");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount11");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
+function buynoww12(){
+    var t= document.getElementById("itemname12");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Package Name: "+name;
+    var s= document.getElementById("itemamount12");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Package Amount: "+amount;
+}
